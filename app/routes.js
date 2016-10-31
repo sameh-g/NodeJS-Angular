a22dbd7269ea8a5dea94687b1fbabddb99b89b59
Sameh George <sameh.george@itworx.com>
2016-10-31 16:53:39 +0200

    update angular routing process..

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,10 @@ module.exports = function(app) {
 
 	// server routes ===========================================================
 	// handle things like api calls
+app.get('/getValue', function(req, res) {
+
+		res.sendfile('./public/login.html');
+});
 	// authentication routes
 
 	// frontend routes =========================================================
--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -15,9 +15,46 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 
 		.when('/geeks', {
 			templateUrl: 'views/geek.html',
-			controller: 'GeekController'	
-		});
+			controller: 'GeekController'
+		}).otherwise({ redirectTo: '/' });
+
+			// 	.when('/geeks', {
+			// templateUrl: 'views/geek.html',
+			// controller: 'GeekController',
+			// resolve: {
+            //  factory: checkRouting
+            //     }	
 
 	$locationProvider.html5Mode(true);
 
 }]);
+
+
+
+
+var checkRouting= function ($q, $rootScope, $location,$http) {
+	console.log('angular authenticate')
+	 var deferred = $q.defer();
+			    $http.get("/getvalue")
+            .success(function (response) {
+				     $location.path("/login.html");
+					     deferred.resolve(true);
+               
+            })
+
+    // if ($rootScope.userProfile) {
+    //     return true;
+    // } else {
+    //     var deferred = $q.defer();
+    //     $http.post("/loadUserProfile", { userToken: "blah" })
+    //         .success(function (response) {
+    //             $rootScope.userProfile = response.userProfile;
+    //             deferred.resolve(true);
+    //         })
+    //         .error(function () {
+    //             deferred.reject();
+    //             $location.path("/");
+    //          });
+    //     return deferred.promise;
+    // }
+};
